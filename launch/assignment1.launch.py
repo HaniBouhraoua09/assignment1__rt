023fb69788886a3e6d13bf02114e67e704d0e3f7
Hani Bouhraoua <hanibouhraoua09@gmail.com>
2025-12-03 13:26:46 +0100

    Updated version: Added Launch file, Run only in  (2 Terminals)

---
+++ launch/assignment1.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # 1. Start the Simulator (Turtlesim)
+        Node(
+            package='turtlesim',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        # 2. Run the Spawner (Sets up the two turtles)
+        Node(
+            package='assignment1_rt',
+            executable='spawner',
+            name='spawner'
+        ),
+        # 3. Start the Distance Monitor
+        Node(
+            package='assignment1_rt',
+            executable='distance_node',
+            name='distance_monitor',
+            output='screen' # This allows you to see the "Too Close!" warnings
+        ),
+    ])
